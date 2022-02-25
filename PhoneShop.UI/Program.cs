5c88142d30663a7e3feb8584f8b563b2a9103b82
Wiktor <wiktorzielak@gmail.com>
2022-02-25 17:16:41 +0100

    added serilog

--- PhoneShop.UI/Program.cs
+++ PhoneShop.UI/Program.cs
@@ -10,19 +10,43 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
-
+using Serilog;
 
 namespace PhoneShop.UI
 {
     public class Program
     {
-        public static async Task Main(string[] args)
-        {  
-            CreateWebHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();        
+
+            try
+            {
+                Log.Information("Starting up!");
+
+                CreateWebHostBuilder(args).Build().Run();
+
+                Log.Information("Stopped cleanly");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
