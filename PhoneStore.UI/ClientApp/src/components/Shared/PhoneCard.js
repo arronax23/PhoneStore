3601716ebfd5a27900ccaf323dd546f52aabb622
Wiktor Zielak <wiktorzielak@gmail.com>
2025-08-08 12:01:36 +0200

    correct css of address tag and pagination container

--- PhoneStore.UI/ClientApp/src/components/Shared/PhoneCard.js
+++ PhoneStore.UI/ClientApp/src/components/Shared/PhoneCard.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
 import Typography from '@material-ui/core/Typography'
-import CardMedia from '@material-ui/core/CardMedia'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import { useHistory } from 'react-router'
@@ -20,7 +18,7 @@ function PhoneCard({ phone }) {
                     <Typography color="textSecondary">
                         {phone.brand} {phone.model}
                     </Typography>
-                    <img className="phone-card-img" src={phone.imageUrl} />
+                    <img className="phone-card-img" alt="phone-image" src={phone.imageUrl} />
                     <Button onClick={handleClick} color="primary" size="small">Show details</Button>
                 </CardContent>
             </Card>
